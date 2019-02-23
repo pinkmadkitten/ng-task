b852cc1b731c2d9c188597c2c62e4589a707b598
Olha Holysh <olgaolha89@ukr.net>
2019-02-23 12:31:04 +0200

    add course service

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import {Component, DoCheck, OnChanges, OnDestroy, OnInit} from '@angular/core';
 import {CourseInterface} from './course/course.interface';
+import {CoursesService} from '../courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -10,39 +11,11 @@ export class CoursesComponent implements OnInit, OnChanges, DoCheck, OnDestroy {
   coursesList: CourseInterface[];
   defaultCourseList: CourseInterface[];
 
-  constructor() {
+  constructor(private courseService: CoursesService) {
   }
 
   ngOnInit() {
-    this.coursesList = [
-      {
-        id: 0,
-        description: 'meow meow meow meow meow meow',
-        creationDate: new Date('02/18/2019'),
-        duration: 26,
-        title: 'Dog World',
-        color: 'lightblue',
-        topRated: true
-      },
-      {
-        id: 1,
-        description: 'meow? meow meow! meow? meow meow! meow? meow meow!',
-        creationDate: new Date('01/25/2019'),
-        duration: 38,
-        title: 'Cat Meow',
-        color: 'pink',
-        topRated: true
-      },
-      {
-        id: 2,
-        description: 'meow meow meow! meow meow meow! meow meow meow! meow meow meow!',
-        creationDate: new Date('03/21/2019'),
-        duration: 125,
-        title: 'Cat World!',
-        color: 'grey',
-        topRated: false
-      }
-    ];
+    this.coursesList = this.courseService.getCourses();
     this.defaultCourseList = [...this.coursesList];
     console.log('onInit');
   }
